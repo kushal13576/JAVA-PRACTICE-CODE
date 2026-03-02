98b156610d71f84da0a15ed3f2d8a3da28830295
KUSHAL SHARMA <kushalkumar65707@gmail.com>
2026-03-02 10:59:46 +0530

    Add files via upload

---
+++ JAVA-FOR-LOOP/CountSetBits.java
@@ -0,0 +1,14 @@
+public class CountSetBits { 
+   public static void main(String[] args) { 
+       int n = 23; 
+       int count = 0; 
+ 
+       for(; n > 0; n >>= 1) { 
+           if((n & 1) == 1) { 
+               count++; 
+           }
+              } 
+ 
+       System.out.println("Set bits: " + count); 
+   } 
+}
---
+++ JAVA-FOR-LOOP/MultiplesOf3.java
@@ -0,0 +1,7 @@
+public class MultiplesOf3 {
+     public static void main(String[] args) { 
+       for(int i = 3; i <= 30; i += 3) { 
+           System.out.print(i + " "); 
+       } 
+   } 
+} 
